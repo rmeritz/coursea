b062f1c5550ed6609003a75b75cf3f097f7ba3e3
Rebecca Meritz <rebecca@meritz.com>
2017-01-07 16:48:39 -0800

    Starting to implement karatsuba

--- algorithms-divide-conquer/karatsuba.rb
+++ algorithms-divide-conquer/karatsuba.rb
@@ -13,10 +13,32 @@
 #[TIP: before submitting, first test the correctness of your program on some small test cases of your own devising. Then post your best test cases to the discussion forums to help your fellow students!]
 
 def karatsuba n, m
-  n.to_i * m.to_i
+  len = n.length
+  if len == 2
+    n.to_i * m.to_i
+  else
+    a = n[0..(len/2 -1)]
+    puts "a = #{a} (56)"
+    b = n[(len/2)..len]
+    puts "b = #{b} (78)"
+    c = m[0..(len/2 - 1)]
+    puts "c = #{c} (12)"
+    d = m[(len/2)..len]
+    puts "d = #{d} (34)"
+
+    step_1 = karatsuba a, c
+    step_2 = karatsuba b, d
+    step_3 = karatsuba((a + b), (c + d))
+    step_4 = step_3 - step_2 - step_1
+    ((10 ** len) * step_1) + step_2 + ((10 ** (len/2)) * step_4)
+  end
 end
 
 y = "5678"
 x = "1234"
 
-puts karatsuba(x, y)
+puts karatsuba(y, x)
+
+#TODO:
+# - Does the algorithim work if n and m are not the same lenght?
+# - What happens in len is odd?
