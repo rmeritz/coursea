1628ad81b21f634e7ec51610dc39b177d3f89689
Rebecca Meritz <rebecca@meritz.com>
2017-01-23 15:04:18 -0800

    Brute Force Multiplication works for only single digits

--- algorithms-divide-conquer/brute_force_multiplication.rb
+++ algorithms-divide-conquer/brute_force_multiplication.rb
@@ -1,7 +1,34 @@
 #!/usr/bin/env ruby
 
-def multiply a, b
-  a * b
+def to_digits n
+  n.to_s.chars.map(&:to_i)
 end
 
-puts multiply 5, 7
+def total_subproducts sum, subproducts, factor
+  if subproducts.empty?
+    sum
+  else
+    total_subproducts(sum + (subproducts.shift * factor), subproducts, factor * 10)
+  end
+end
+
+def sum_subproducts subproducts
+  total_subproducts 0, subproducts, 1
+end
+
+def calculate_subproduct i, b
+  i * b.join.to_i
+end
+
+def calculate_subproducts a, b
+  a.map { |i| calculate_subproduct i, b }
+end
+
+def multiply a, b
+  sum_subproducts(calculate_subproducts(to_digits(a), to_digits(b)))
+end
+
+b = 12312315
+n = 9
+puts multiply n, b
+puts n * b
