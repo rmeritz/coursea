c06e106bd1dc52a3360270f05cf2afaba666077f
Rebecca Meritz <rebecca@meritz.com>
2017-01-23 16:10:42 -0800

    Place methods on a class

--- algorithms-divide-conquer/brute_force_multiplication.rb
+++ algorithms-divide-conquer/brute_force_multiplication.rb
@@ -3,53 +3,57 @@
 gem 'minitest'
 require "minitest/autorun"
 
-def to_digits n
-  n.to_s.chars.map(&:to_i)
-end
 
-def total_subproducts sum, subproducts, factor
-  if subproducts.empty?
-    sum
-  else
-    total_subproducts(sum + (subproducts.pop * factor), subproducts, factor * 10)
+class Multiplication
+  def multiply a, b
+    sum_subproducts(calculate_subproducts(to_digits(a), to_digits(b)))
   end
-end
 
-def sum_subproducts subproducts
-  total_subproducts 0, subproducts, 1
-end
-
-def carry_over_and_new_digit product
-  prod_mod = product % 10
-  if prod_mod == product
-    carry_over = 0
-    new_digit = product
-  else
-    carry_over = product / 10
-    new_digit = prod_mod
+  private
+  def to_digits n
+    n.to_s.chars.map(&:to_i)
   end
-  [carry_over, new_digit]
-end
 
-def calc_subproduct i, b, carry_over, subproduct
-  if b.empty?
-    subproduct.unshift(carry_over).join.to_i
-  else
-    carry_over, new_digit = carry_over_and_new_digit((i * b.pop) + carry_over)
-    calc_subproduct(i, b, carry_over, subproduct.unshift(new_digit))
+  def sum_subproducts subproducts
+    total_subproducts 0, subproducts, 1
   end
-end
 
-def calculate_subproduct i, b
-  calc_subproduct(i, b, 0, [])
-end
+  def total_subproducts sum, subproducts, factor
+    if subproducts.empty?
+      sum
+    else
+      total_subproducts(sum + (subproducts.pop * factor), subproducts, factor * 10)
+    end
+  end
 
-def calculate_subproducts b, a
-  a.map { |i| calculate_subproduct i, b.clone }
-end
+  def calculate_subproducts b, a
+    a.map { |i| calculate_subproduct i, b.clone }
+  end
 
-def multiply a, b
-  sum_subproducts(calculate_subproducts(to_digits(a), to_digits(b)))
+  def calculate_subproduct i, b
+    calc_subproduct(i, b, 0, [])
+  end
+
+  def calc_subproduct i, b, carry_over, subproduct
+    if b.empty?
+      subproduct.unshift(carry_over).join.to_i
+    else
+      carry_over, new_digit = carry_over_and_new_digit((i * b.pop) + carry_over)
+      calc_subproduct(i, b, carry_over, subproduct.unshift(new_digit))
+    end
+  end
+
+  def carry_over_and_new_digit product
+    prod_mod = product % 10
+    if prod_mod == product
+      carry_over = 0
+      new_digit = product
+    else
+      carry_over = product / 10
+      new_digit = prod_mod
+    end
+    [carry_over, new_digit]
+  end
 end
 
 class MultiplicationTest < Minitest::Test
@@ -59,6 +63,6 @@ class MultiplicationTest < Minitest::Test
   end
 
   def test_multiplication
-    assert_equal multiply(@n, @b), @n * @b
+    assert_equal Multiplication.new.multiply(@n, @b), @n * @b
   end
 end
