fea40b03b7a20bd6688e7a3f1291e142bb885d30
Rebecca Meritz <rebecca@meritz.com>
2017-03-30 12:55:48 -0600

    Use rubys built in matrix to build basic tests

--- algorithms-divide-conquer/matrix_multiplication.rb
+++ algorithms-divide-conquer/matrix_multiplication.rb
@@ -2,9 +2,10 @@
 
 gem 'minitest'
 require "minitest/autorun"
+require "matrix"
 
 
-class MatirxMultiplication
+class MatrixMultiplication
 
   def brute_force_matrix_multiply a, b
   end
@@ -15,14 +16,22 @@ class MatirxMultiplication
   private
 end
 
-class MatirxMultiplicationTest < Minitest::Test
+class MatrixMultiplicationTest < Minitest::Test
   def setup
-    @matrix_multiplication = MatirxMultiplication.new
+    @matrix_multiplication = MatrixMultiplication.new
+    @m1 = Matrix[[1, 2], [3, 4]]
+    @m2 = Matrix[[5, 6], [7, 8]]
   end
 
   def test_brute_force_matrix_multiplication
+    assert_equal(@m1 * @m2,
+                 @matrix_multiplication.brute_force_matrix_multiply(@m1, @m2)
+                )
   end
 
   def test_strassen_subcubic_matrix_multiplication
+    assert_equal(@m1 * @m2,
+                 @matrix_multiplication.strassen_subcubic_matrix_multiply(@m1, @m2)
+                )
   end
 end
